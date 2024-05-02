36eae037592a3790db96b7c575633e20ee7085a5
EmptyChair <romaswebex01@gmail.com>
2024-05-02 19:24:37 +0200

    update

---
+++ u22_2024_04_29/HA_u21/u21_script.js
@@ -0,0 +1,89 @@
+//synchronous code
+console.log("Start");
+
+//asynchronous code - launch the function in 7 seconds, show 10 on console
+setTimeout(() => console.log(10), 7000);
+
+//synchronous code
+console.log("Finish");
+
+//NOTE: synchronous code will be executed before asynchronous code!!!
+//такое спрашивают на собеседованиях
+
+function giveFiveAfterSevenSeconds() {
+    setTimeout(() => 5, 7000);  
+}
+
+function giveFiveAfterZeroSeconds() {
+    setTimeout(() => 5, 0);  
+}
+
+//result will be undefined because of  the delay
+//THIS IS A PROBLEM
+let res = giveFiveAfterSevenSeconds();
+console.log(res)
+let res2 = giveFiveAfterZeroSeconds();
+console.log(res2)
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(5), 7000);
+
+})
+
+
+promise
+    .then((value) => console.log(value)) //return parameter if positive outcome (resolve)
+    .catch((err) => console.log(err)) //return parameter if negative outcome (reject)
+    .finally(console.log("Thank you for using the app!")); //will be executed no matter what
+
+const usersList = document.getElementById("usersList");
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then(users => {
+        users.forEach(e => {
+            const li = document.createElement("li");
+            li.textContent = `${e.name}`; 
+            usersList.appendChild(li); 
+        });
+    })
+//response provides information about the response, not response itself :.then((response) => console.log(response)) 
+
+const postsList = document.getElementById("postsList");
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then(posts => {
+        posts.forEach(post => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <h3>${post.id}</h3>
+                <p> </p>
+                ${post.title} <br> ${post.body};
+                
+            `;
+            postsList.appendChild(li);
+
+
+            /* MY VERSION
+            const l1 = document.createElement("l1");
+            l1.textContent = `UserID: ${e.userId}`;
+            const l2 = document.createElement("l2");
+            l2.textContent = `PostID: ${e.id}`;
+            const l3 = document.createElement("l3");
+            l3.textContent = `Title: ${e.title}`;
+            const l4 = document.createElement("l4");
+            l4.textContent = `Text: ${e.body}`;
+            l1.classList.add("d-flex", "mb-1");
+            l2.classList.add("d-flex", "mt-3", "text-dark", "fw-bold");
+            l3.classList.add("d-flex", "mb-1");
+            l4.classList.add("d-flex", "mb-1");
+           
+                          
+            postsList.appendChild(l2); 
+            postsList.appendChild(l1); 
+            postsList.appendChild(l3); 
+            postsList.appendChild(l4); 
+            */ 
+        });
+    })
---
+++ u23_2024_04_30/HA_u22/u22_script.js
@@ -0,0 +1,70 @@
+const BASE_URL = "https://jsonplaceholder.typicode.com/";
+//such named constants are supposed to be actually constant
+
+async function fetchUsers() {
+    try {
+        // await - wait till data arrives
+        const response = await fetch(`${BASE_URL}/users`); 
+        const users = await response.json();
+        return users;
+    } catch (error) {
+        console.log(error.message);
+    }    
+}
+
+function displayUsers(users) {
+    users.forEach((user) => {
+        const li = document.createElement("li");
+        li.textContent = user.name;
+        li.onclick = showInfo;
+        function showInfo() {
+            displayIndividual(user);
+        }        
+        /*
+        const infoButton = document.createElement('button'); 
+        infoButton.innerHTML = "?";
+        infoButton.classList.add("btn", "btn-info", "ms-2");
+        infoButton.onclick = showInfo;
+        function showInfo() {
+            displayIndividual(user);
+        }        
+        li.appendChild(infoButton);  
+        */
+        usersList.appendChild(li);       
+    });
+}
+
+const result = async () => {
+    const users = await fetchUsers();
+    if (users) {
+        displayUsers(users);
+    } else {
+        console.log("No users found!");
+    }
+    
+}
+
+function displayIndividual(user) {
+    const data = document.createElement("data");
+    data.innerHTML = `
+    <h3>${user.name}</h3>
+    <h5>Email: ${user.email}</h5>
+    <h5>Phone: ${user.phone}</h5>
+    <h5>Website: ${user.website}</h5>
+    <h5>Company Name: ${user.company.name}</h5>
+    <h5>Address: ${user.address.street},  ${user.address.suite},  ${user.address.city}</h5>
+    `;
+    if (userContent.childNodes.length == 0) {
+        userContent.appendChild(data);
+    } else {
+        userContent.removeChild(userContent.firstChild);
+        userContent.appendChild(data);
+    }
+    
+}
+
+
+
+result();
+console.log(userContent.childNodes.length)
+//console.log(displayUsers(fetchUsers()));
