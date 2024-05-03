d228d54615b2fbb3453816fac8848f5eb5237528
EmptyChair <romaswebex01@gmail.com>
2024-05-03 10:36:14 +0200

    update

--- u23_2024_04_30/u23_script.js
+++ u23_2024_04_30/u23_script.js
@@ -14,17 +14,6 @@ async function fetchImages() {
     }
 }
 
-next.onclick = getNextImage;
-prev.onclick = getPrevImage;
-
-async function getNextImage() {
-    getImage(1);
-}
-
-async function getPrevImage() {
-    getImage(-1);
-}
-
 async function getImage(x) {
     const images = await fetchImages();
     console.log("Fetched Images: ");
@@ -42,3 +31,49 @@ async function getImage(x) {
     console.log("Id from new count: "+newID);
     image.src = images[newID].thumbnailUrl;
 }
+
+async function getNextImage() {
+    getImage(1);
+}
+
+async function getPrevImage() {
+    getImage(-1);
+}
+
+next.onclick = getNextImage;
+prev.onclick = getPrevImage;
+
+/*
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+const image = document.getElementById("image");
+let count = 1;
+
+async function fetchImages() {
+    try {
+        const response = await fetch(`${BASE_URL}/photos`)
+        const images = await response.json();
+        return images;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+next.onclick = getImage(1);
+prev.onclick = getImage(-1);
+
+function getImage(x) {
+    const images = fetchImages();
+    console.log("Reveal fetched images:");
+    console.log(images);
+    //determine the image ID we need
+    count = count+x;
+    if (count<1) {
+        count = 5000 
+    }
+    if (count > 5000) {
+        count = 0;
+    }
+    newID = count-1;
+    image.src = images[newID].thumbnailUrl;
+}
+*/
